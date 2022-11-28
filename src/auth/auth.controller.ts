a89aca401de21fe7c78b41eca419937cdbe9177f
Joe Kariuki <jknguru6@gmail.com>
2022-11-28 12:00:36 +0300

    Format auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,18 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {
-    }
+  constructor(private authService: AuthService) {}
 
-    @Post('signup')
-    signup(@Body() dto: AuthDto) {
-        console.log({
-            dto,
-        })
-       return this.authService.signup()
-    }
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup();
+  }
 
-    @Post('signin')
-    signin(){
-        return this.authService.signin()
-    }
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
 }
