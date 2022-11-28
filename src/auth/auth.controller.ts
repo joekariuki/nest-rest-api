edd67da362e32801828118b80d3f8da98944a602
Joe Kariuki <jknguru6@gmail.com>
2022-11-28 11:45:45 +0300

    Import auth dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -8,7 +8,7 @@ export class AuthController {
     }
 
     @Post('signup')
-    signup(@Body() dto: any) {
+    signup(@Body() dto: AuthDto) {
         console.log({
             dto,
         })
