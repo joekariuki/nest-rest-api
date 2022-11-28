89f2af4218889129e0d2ca9dcb50ce9f72a53adb
Joe Kariuki <jknguru6@gmail.com>
2022-11-28 12:16:38 +0300

    Add user creation logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup(dto: AuthDto) {
-    return { msg: 'I have signed up' };
+  async signup(dto: AuthDto) {
+    // Generate password hash
+    const hash = await argon.hash(dto.password);
+    // Save user in db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    // Return saved user
+    return user;
   }
 
   signin() {
