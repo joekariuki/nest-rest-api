3b690da3a05526c598019f2c338f7e83464cfe14
Joe Kariuki <jknguru6@gmail.com>
2022-11-28 12:11:56 +0300

    Add auth dto to signup function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
 
-    constructor(private prisma: PrismaService) {}
-    
-    signup() {
-        return { msg: 'I have signed up'}
-    }
+  signup(dto: AuthDto) {
+    return { msg: 'I have signed up' };
+  }
 
-    signin() {
-        return { msg: 'I have signed in'}
-    }
+  signin() {
+    return { msg: 'I have signed in' };
+  }
 }
